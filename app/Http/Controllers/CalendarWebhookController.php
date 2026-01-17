7d40230e542615c6eebe7e94803db5ca868d3cdb
Hina524 <132186412+Hina524@users.noreply.github.com>
2026-01-17 13:32:51 +0900

    Webhookハンドラーの実装

--- app/Http/Controllers/CalendarWebhookController.php
+++ app/Http/Controllers/CalendarWebhookController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\WatchChannel;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class CalendarWebhookController extends Controller
 {
-    //
+    public function handle(Request $request): Response
+    {
+        $channelId = $request->header('X-Goog-Channel-ID');
+        $resourceState = $request->header('X-Goog-Resource-State');
+
+        Log::info('Calendar webhook received', [
+            'channel_id' => $channelId,
+            'resource_state' => $resourceState,
+        ]);
+
+        if ($resourceState === 'sync') {
+            return response('OK', 200);
+        }
+
+        $watchChannel = WatchChannel::where('channel_id', $channelId)->first();
+
+        if (!$watchChannel) {
+            Log::warning('Unknown channel ID', ['channel_id' => $channelId]);
+            return response('OK', 200);
+        }
+
+        return response('OK', 200);
+    }
 }
